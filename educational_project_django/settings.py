4b85103c1d59dc8ef1e7f9a1cca9a1e8edc01466
Alexander Morozov <reverse43@gmail.com>
2022-05-26 20:24:29 +0300

    Delete folder created by mistake

--- educational_project_django/settings.py
+++ educational_project_django/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'landing_page.apps.LandingPageConfig',
 
     'crm',
+    'cms'
 ]
 
 MIDDLEWARE = [
