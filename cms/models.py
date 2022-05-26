7326d0c843cee7fcd859ed0f4ef9ad971a69a3d2
Alexander Morozov <reverse43@gmail.com>
2022-05-26 20:34:39 +0300

    Add CMS app

---
+++ cms/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Slider(models.Model):
+    img = models.ImageField(upload_to='slider_img/')
+    title = models.CharField('Заголовок', max_length=200)
+    text = models.TextField('Текст')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Слайд'
+        verbose_name_plural = 'Слайды'
