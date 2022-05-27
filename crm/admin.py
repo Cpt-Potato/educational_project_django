179da223e8fd256ccff24f13a220820ffe0f8f35
Alexander Morozov <reverse43@gmail.com>
2022-05-27 22:33:53 +0300

    Add CRM app models to admin

--- crm/admin.py
+++ crm/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from crm.models import Order
+from crm.models import Order, Status, Comment
 
 admin.site.register(Order)
+admin.site.register(Status)
+admin.site.register(Comment)
--- crm/models.py
+++ crm/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Status(models.Model):
-    status = models.CharField(max_length=200, verbose_name='Название статуса')
+    status = models.CharField('Название статуса', max_length=200)
 
     def __str__(self):
         return self.status
