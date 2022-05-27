792f03ba03ff4be12ee43dbb127fc3eb99c7afc2
Alexander Morozov <reverse43@gmail.com>
2022-05-27 22:32:56 +0300

    Use django forms

--- crm/views.py
+++ crm/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from cms.models import Slider
 from price.models import Card, Service
+from .forms import OrderForm
 from .models import Order
 
 
@@ -9,21 +10,21 @@ def index(request):
     slides = Slider.objects.all()
     card_1, card_2, card_3 = Card.objects.all()
     services = Service.objects.all()
+    form = OrderForm()
     context = {
         'slides': slides,
         'card_1': card_1,
         'card_2': card_2,
         'card_3': card_3,
-        'services': services
+        'services': services,
+        'form': form
     }
     return render(request, 'pages/index.html', context)
 
 
 def thanks(request):
-    if request.POST:
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        element = Order(order_name=name, order_phone=phone)
-        element.save()
-    else:
-        return render(request, "pages/thanks.html")
+    name = request.POST.get("name")
+    phone = request.POST.get("phone")
+    element = Order(order_name=name, order_phone=phone)
+    element.save()
+    return render(request, "pages/thanks.html", {'name': name})
