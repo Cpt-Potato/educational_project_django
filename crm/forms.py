b48329576f2e20e719d2e32b71bb0ddd88e9e271
Alexander Morozov <reverse43@gmail.com>
2022-05-28 18:08:22 +0300

    Add required fields

--- crm/forms.py
+++ crm/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class OrderForm(forms.Form):
     name = forms.CharField(
-        max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'})
+        max_length=200, required=True, widget=forms.TextInput(attrs={'class': 'form-control'})
     )
     phone = forms.CharField(
-        max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'})
+        max_length=200, required=True, widget=forms.TextInput(attrs={'class': 'form-control'})
     )
